1014de4ccd611b60213aacbef30fb4f93b6a66f0
abhishek-rai-1 <abhishekrai1972@gmail.com>
2025-06-02 19:03:15 +0530

    add Annotation and TypeInfference

---
+++ 1. Annotation/index.ts
@@ -0,0 +1,14 @@
+// string
+
+let myName: string = "abhishek";
+myName = "raj";
+console.log(myName);
+
+// number
+let favNumber: number = 12;
+console.log(favNumber);
+
+// boolean
+let isTsHard: boolean = false;
+isTsHard = true;
+console.log(isTsHard);
---
+++ 2. TypeInference/index.ts
@@ -0,0 +1,5 @@
+let tech = 'TypeScript'
+
+console.log(tech);
+
+// tech = 12   // error as we can't assign number to string
