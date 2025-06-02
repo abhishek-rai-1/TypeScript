ebd16f32b642db6b86d3ff189d679993c5561061
abhishek-rai-1 <abhishekrai1972@gmail.com>
2025-06-02 19:20:04 +0530

    create funtion Parameter Annotation

---
+++ functionParameterAnnotation.ts
@@ -0,0 +1,14 @@
+// regular function
+
+function addOne(num: number) {
+  return num + 1;
+}
+
+const res = addOne(5);
+console.log(res);
+
+// arrow function
+let multiply = (x: number, y: number) => x * y;
+
+let result = multiply(2, 5);
+console.log(result);
