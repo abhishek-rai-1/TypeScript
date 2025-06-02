0130d5fb92d832e0e97d42220406b2892dcccdc7
abhishek-rai-1 <abhishekrai1972@gmail.com>
2025-06-02 19:08:37 +0530

    change Inference Content

--- 2. TypeInference/index.ts
+++ 2. TypeInference/index.ts
@@ -1,5 +1,10 @@
 let tech = 'TypeScript'
+let favnumber = 23;
+let isHard = false;
 
 console.log(tech);
 
 // tech = 12   // error as we can't assign number to string
+
+console.log(favnumber);
+console.log(isHard);
