045b80e63c365a2f18e74ac8f8ffffd205934cc5
abhishek-rai-1 <abhishekrai1972@gmail.com>
2025-06-03 18:00:52 +0530

    create Tuples

---
+++ 14. Tuples.ts
@@ -0,0 +1,8 @@
+let myTuples : [string, number] = ['raj', 23];
+// console.log(myTuples[0])
+// console.log(myTuples[1])
+
+let [first, second] = myTuples;
+
+console.log(first);
+console.log(second);
