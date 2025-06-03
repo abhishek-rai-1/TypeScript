4e992765bcccdb8523c8f6afe6f48eb975cc83a9
abhishek-rai-1 <abhishekrai1972@gmail.com>
2025-06-03 17:38:38 +0530

    Literals

---
+++ 13. LiteralType.ts
@@ -0,0 +1,9 @@
+// string literal
+let color : 'red' | 'green' | 'blue';
+color = 'red';  // valid
+// color = 'black'     invalid
+
+// number literal
+let number : 1 | 2 | 3;
+number = 1  // valid
+// number = 5  invalid
